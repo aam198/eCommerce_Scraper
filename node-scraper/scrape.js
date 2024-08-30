aad534718740ba99bd53c44be10b8399ded83979
aam198 <aam198@psu.edu>
2024-08-30 14:02:55 -0400

    Added notes from testing additional urls

--- node-scraper/scrape.js
+++ node-scraper/scrape.js
@@ -6,10 +6,21 @@ const scrape = async() => {
   const page = await browser.newPage();
 
   // Initial page to search
+  // const url = 'https://wegmans.com';
   const url = 'https://books.toscrape.com';
+  // Gives: Attention Required! | Cloudflare
+  // const url = 'https://www.weismarkets.com/';
+  // GiantFoods is giving: Site Temporarily Down
   // const url = 'https://giantfoodstores.com';
 
   await page.goto(url);
+
+
+  // Evaluate method allows to run javascript in the context of the page and select data using query selector etc
+  const books = await page.evaluate(() => {
+    // Get the element that surrounds the data
+  });
+
 
   // Select what we want
   // Getting the title
