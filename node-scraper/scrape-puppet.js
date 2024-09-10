4835dc2443ba2963e6dce4623e47645cad13aff3
aam198 <aam198@psu.edu>
2024-09-10 14:46:23 -0400

    Book selectors added to get title and price

--- node-scraper/scrape-puppet.js
+++ node-scraper/scrape-puppet.js
@@ -1,13 +1,28 @@
 import puppeteer from "puppeteer";
 
 const scrape = async () => {
-  const browser =await puppeteer.launch({headless:false});
+  // Launch Headless Browser
+  const browser = await puppeteer.launch({headless:true});
   const page = await browser.newPage();
 
   const url = 'https://books.toscrape.com';
 
+  await page.goto(url);
+
+  const books = await page.evaluate(() => {
+    const bookElements = document.querySelectorAll('.product_pod');
+    // Empty objects into an array of each product_pod
+    return Array.from(bookElements).map((book) => {
+      const title = book.querySelector('h3 a').getAttribute('title');
+      const price = book.querySelector('.price_color').textContent;
+      return price;
+    })
+  });
+
+  console.log(books);
 
   await browser.close();
 };
 
+// Call the Function to start scrape
 scrape();
