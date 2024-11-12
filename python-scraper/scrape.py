c58eb91d0039d01f46276b9a32df072acc70e4d6
aam198 <aam198@psu.edu>
2024-11-12 14:35:33 -0500

    python imports added

--- python-scraper/scrape.py
+++ python-scraper/scrape.py
@@ -2,3 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+def fetch_books(page_number):
+  print(page_number)
+
+
+def main():
+  fetch_books(1)
+
+
+if __name__ == "__main__":
+    main()
