b070a7af132f4d71ab02ad9c30e1ea62e5c7d5fe
aam198 <aam198@psu.edu>
2025-01-03 09:41:25 -0500

    Build out of fetch_books function

--- python-scraper/scrape.py
+++ python-scraper/scrape.py
@@ -3,9 +3,16 @@ from bs4 import BeautifulSoup
 import json
 
 def fetch_books(page_number):
-  print(page_number)
+  url = f"https://books.toscrape.com/catalogue/page-{page_number}.html"
+  # To get response 
+  response = requests.get(url)
+  # Create an object
+  soup = BeautifulSoup(response.text, 'html.parser')
 
+  # Test, which will print out the entire website in html format
+  print(soup.prettify())
 
+# Entry point
 def main():
   fetch_books(1)
 
